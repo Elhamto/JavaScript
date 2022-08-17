894aaf9a56a7938bf307881e54ce045a61101ebf
Elhamto <elham.toloeii@gmail.com>
2022-08-17 14:19:41 +0430

    p2 addd

--- w3/w3p1.js
+++ w3/w3p1.js
@@ -16,25 +16,27 @@ function digit(sum){
         return str
     }
 }
-console.log(digit(10));
+console.log("raveshe1: ",digit(10));
 
 
 
-/******* ba reshte (vali revers nadare) ********/
-
-// let sum= 8
-// let str=""
-// if (sum<10) {
-//     str+= sum
-// }
-// else{
-//     for (let i = 9; i > 0; i--) {
-//         sum -=i
-//         str+=i
-//         if (sum<10) {
-//             str+= sum
-//             break
-//         }
-//     }
-// }
-// console.log(str);
+/******* ba reshte  ********/
+function digits(sum) {
+    if (sum<10) {
+        return sum
+    }
+    else{
+        let str=""
+        for (let i = 9; i > 0; i--) {
+            sum -=i
+            str+=i
+            if (sum<i) {
+                str+= sum
+                break
+            }
+        }
+        str = [...str].reverse().join("");
+        return str
+    }
+}
+console.log("raveshe2: ",digits(10));
